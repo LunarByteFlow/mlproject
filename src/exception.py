7558e083a2a166785c39d9334c807d8bcc4329f8
Mahnoor Tauseef <mahnoortauseef2022@gmail.com>
2024-10-19 02:36:15 +0500

    Data Injestion

--- src/exception.py
+++ src/exception.py
@@ -1,6 +1,7 @@
 # The sys module in python provides various functions and variables that are used to manipulate different parts of the python runtime environment
 import sys
 import logging
+# from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb = error_detail.exc_info()
@@ -18,12 +19,7 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
 
-if __name__ == "__main__":
-    try:
-        a = 1/0
-    except Exception as e:
-        logging.info("Devide by zero error")
-        raise CustomException(e,sys)
+
 
 
 
