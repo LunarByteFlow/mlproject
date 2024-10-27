e90ce88337386e858d2d63f9d5eaa6bef9599e7f
LunarByteFlow <141225732+LunarByteFlow@users.noreply.github.com>
2024-10-27 13:48:45 +0500

    Update application.py

--- application.py
+++ application.py
@@ -32,7 +32,7 @@ def predict_datapoint():
         return render_template('home.html',results = results[0])
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',Debug_mode="True")
+    app.run(host='0.0.0.0')
 
 
 
